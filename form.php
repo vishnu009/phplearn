15aa2dcae19cc37ed6f80838e3727f76d4167346
vishnu <vishnu@ubuntu.ubuntu-domain>
2012-09-28 17:29:30 +1000

    Added metro

---
+++ form.php
@@ -0,0 +1,25 @@
+<!Doctype html>
+<meta charset="utf8">
+<head>
+<title>PHP form actions </title>
+<style type="text/css">
+body{
+	display: inline-block;
+}
+</style>
+</head>
+<body>
+	<form action="input.php" method="post">
+		<label>Enter your  First Name :
+			<input type="text" id="fname" name="fname"/>
+			</label><br/>
+			<label>Enter your Last Name :
+			<input type="text" id="lname" name="lname"/><br/>
+			</label>
+			<input type="submit" value="Go" />
+			</form>
+
+</body>
+
+
+
