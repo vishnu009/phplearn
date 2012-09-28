9044cbbfc7bda9c608b8ea21f8f3159b984ffc58
vishnu <vishnu@ubuntu.ubuntu-domain>
2012-09-28 16:04:24 +1000

    first commit so far

---
+++ three.php
@@ -0,0 +1,13 @@
+<!Doctype html>
+<meta charset ="utf-8">
+<head>
+	<title>progrmming in php </title>
+</head>
+<body>
+	<h1> Welcome to first page of php </h1>
+	<?php 
+	echo date('l \t\h\e jS'); ?>
+</body>
+</html>
+
+
---
+++ two.php
@@ -0,0 +1,9 @@
+<?php
+echo 'php is super';
+echo'<br />';
+$v =100;
+$c =300;
+$z =$v*$c;
+echo "the Product of v multipled by c is ".$z;
+?>
+
