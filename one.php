5386d2b9778252c8bb3b474aafaad86598d25c5a
vishnu <vishnu.kvs@gmail.com>
2012-11-08 11:49:29 +1100

    add one.php

---
+++ one.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<head>
+<title>first Project</title>
+</head>
+<body>
+	<h1>Welcome  for Learing Php</h1>
+	<P> This is  comeback learing PHP </p>
+		<?PHP echo "Hello World"; ?>
+	<div>
+		<?php 
+		$a=100;
+		$b=200;
+		$c= $a+$b;
+		$d=$a*$b;
+		echo "the sum of a +b is $c";
+		echo "the prouct of a and b is $d";
+		?>
+	</div>
+</body>
+</html>
